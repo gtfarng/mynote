e74260c5a572fccb1509d13676376c462596bf08
gtfarng <5735512002@psu.ac.th>
2019-03-29 01:06:29 +0700

    update

---
+++ install_node_js.sh
@@ -0,0 +1,9 @@
+sudo apt install curl
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt install nodejs
+node -v
+npm -v
+npm install -g nodemon
+npm install -g create-react-app
+npm install -g yarn
+
