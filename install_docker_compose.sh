7fe362bfe17617869cab7162092600fd0a6580ee
gtfarng <5735512002@psu.ac.th>
2019-04-09 11:09:57 +0700

    add_shell_install_docker

---
+++ install_docker_compose.sh
@@ -0,0 +1,17 @@
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo docker -v
+
+sudo docker-compose -v
