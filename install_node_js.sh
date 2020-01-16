8da644608255b748101b4eef0425f15a833d2eb6
Jatupat Pannoi <5735512002@psu.ac.th>
2020-01-16 16:38:06 +0700

    Update install_node_js.sh

--- install_node_js.sh
+++ install_node_js.sh
@@ -1,6 +1,6 @@
-sudo apt install curl
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
-sudo apt install nodejs
+sudo apt install curl -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+sudo apt install nodejs -y
 node -v
 npm -v
 npm install -g nodemon
